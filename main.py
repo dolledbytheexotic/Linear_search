923060e839feb83255eb52bb22f5d675156f03ef
Kennedy Djeumi <114368365+Kennedydjeumi@users.noreply.github.com>
2023-01-12 01:32:45 +0000

    Made a coding project where I can tell you photos you've liked and itll show you only the people you follow and liked their pictures at some point will show up. Still some error if you have any suggestions please be welcomed to share.

--- main.py
+++ main.py
@@ -1,58 +1,27 @@
-def divide(x, y):
-    return x / y
+import json  
 
-def multiply(x, y):
-    return x * y
+# Load liked_posts.json
+with open ('liked_posts.json') as file: 
+  likedposts = json.load(file)
 
-def addition(x, y):
-    return x + y
+# Load following.json
+with open('following.json') as file:
+  following_json = json.load(file)
 
-def subtract(x, y):
-    return x - y
+# Create an empty list for liked posts and following users
+liked_post_and_followingusers = []
 
-def exponents(x, y):
-    return x**y
+# Add all following users to the list
+for following in following_json["relationships_following"]: 
+  liked_post_and_followingusers.append(following["string_list_data"][0]["value"])
 
-def square_root(x):
-    import math
-    square_root = (math.sqrt(x))
-    return square_root
+# Add all image users to the list
+for image in likedposts["likes_media_likes"]:
+  liked_post_and_followingusers.append(image["string_list_data"][0]["value"])
 
+# Remove duplicates from the list
+liked_post_and_followingusers = list(set(liked_post_and_followingusers))
 
-print("Choose Math function" )
-application=["1.divide","2.multiply","3.addition","4.subtract","5.exponents","6.Square root"] 
-for x in application:
-  print(x)
-
-
-while True:
-    Choice=input("Enter choice (1/2/3/4/5/6/):")
-    if Choice in ('1','2','3','4','5','6',):
-      num1=float(input("Enter your first number"))
-      num2=float(input("Enter your first number"))
-
-      if Choice=='1':
-        print(num1,"/",num2,"=",divide(num1,num2))
-
-      elif Choice=='2':
-        print(num1,"*",num2,"=",multiply(num1,num2))
-      
-      elif Choice=='3':
-        print(num1,"+",num2,"=",addition(num1,num2))
-
-      elif Choice=='4':
-        print(num1,"-",num2,"=",subtract(num1,num2))
-
-      elif Choice=='5':
-        print(num1,"**",num2,"=",exponents(num1,num2))
-
-      elif Choice=='6':
-        print(num1,"sqrt","=",square_root(num1))
-
-
-      satisfaction=input("do you want to do another calculation? (yes/no):")
-      if satisfaction=="no":
-        break
-
-else:
-  print("Invalid input")
+# Print out the unique list of users
+for user in liked_post_and_followingusers:
+  print(user)
