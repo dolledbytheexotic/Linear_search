2681a1aa017ce42b4ce25095b340944594f8b7e9
Kennedy Djeumi <114368365+Kennedydjeumi@users.noreply.github.com>
2023-01-15 03:07:10 +0000

    Data structures and algorithms

---
+++ linear_search.py
@@ -0,0 +1,31 @@
+def linear_search(list,target):
+
+
+
+
+for i in range(0,len(list)):
+  if list[i]==target:
+
+
+
+
+
+
+
+
+
+
+
+
+def verify(index):
+  if index is not None: 
+    print("Target is found at index",index)
+
+  else :
+    print("Target is not found in list")
+
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+
+result=linear_search(numbers,12)
+verify(result)
--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-import requests
-from bs4 import BeautifulSoup
+#The first line def function uses the term linear_search which calls items and list then we use a for loop in range from 0 to the number of items in list
+def linear_search(list,target):
+  for i in range (0,len(list)):
+    if list[i]==target:
+      return i
+  return None
+#We use a if statement to compare the i in for loop to target and if its equal to target we return none 
 
-# Website 1
-website1 = "website api"
-page1 = requests.get(website1)
-soup1 = BeautifulSoup(page1.content, "html.parser")
-price1 = soup1.find("span", class_="price").get_text(0)
 
-# Website 2
-website2 = "Website api"
-page2 = requests.get(website2)
-soup2 = BeautifulSoup(page2.content, "html.parser")
-price2 = soup2.find("span", class_="price").get_text()
+#In the second function we have verify which takes the place of result which uses the linear search function 
 
-if float(price1) < float(price2):
-    print(f"The price on {website1} is cheaper: {price1}")
-elif float(price1) > float(price2):
-    print(f"The price on {website2} is cheaper: {price2}")
-else:
-    print(f"The prices on both websites are equal: {price1}")
+
+def verify(index):
+  if index is not None: 
+    print("Target is found at index",index)
+
+  else :
+    print("Target is not found in list")
+#checks if numbers has the target and at what index and if it doesn't it prints "target is not found in list"
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+
+result=linear_search(numbers,9)
+verify(result)
